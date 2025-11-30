73eb344e4c11f391d2a0f32cff1dc60c5e347f74
GakuAkita <gaku.medaka@gmail.com>
2025-11-30 21:39:23 +0900

    minsdk is 23

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
 //        minSdk = flutter.minSdkVersion
-        minSdk = flutter.minSdkVersion//23に変更
+        minSdk = 23 //23に変更
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
