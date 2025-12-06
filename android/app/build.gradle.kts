3039f60a44d65b5c242e77f53101e26e59fb8980
GakuAkita <gaku.medaka@gmail.com>
2025-12-06 16:57:46 +0900

    honestly I don't want to this, but I overwrite flutter.minSdkVersion directlyu

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,8 +28,7 @@ android {
         applicationId = "com.gakuakita.little_prince.my_travel_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-//        minSdk = flutter.minSdkVersion
-        minSdk = flutter.minSdkVersion// 23に変更。flutter.minSdkVersion
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
