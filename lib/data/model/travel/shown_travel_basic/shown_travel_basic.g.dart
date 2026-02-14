b643d3b7a67ff4090a0493d2194d44d3e6f92135
GakuAkita <gaku.medaka@gmail.com>
2026-02-14 09:27:50 +0900

    feat: Add ShownTravelBasic model
    Adds the `ShownTravelBasic` data model, which includes `travelId` and `groupId` fields.
    
    This change includes the freezed model definition, along with the generated `.freezed.dart` and `.g.dart` files for JSON serialization.

---
+++ lib/data/model/travel/shown_travel_basic/shown_travel_basic.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'shown_travel_basic.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_ShownTravelBasic _$ShownTravelBasicFromJson(Map<String, dynamic> json) =>
+    _ShownTravelBasic(
+      travelId: json['travelId'] as String?,
+      groupId: json['groupId'] as String?,
+    );
+
+Map<String, dynamic> _$ShownTravelBasicToJson(_ShownTravelBasic instance) =>
+    <String, dynamic>{
+      'travelId': instance.travelId,
+      'groupId': instance.groupId,
+    };
