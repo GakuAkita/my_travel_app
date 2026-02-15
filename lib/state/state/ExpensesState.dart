895fc3cfada3250745a9cfd32379f59d62f78d02
GakuAkita <gaku.medaka@gmail.com>
2026-02-15 10:59:27 +0900

    feat: Listen for travel changes in states
    Inject `ShownTravelSession` into `ExpensesState` and `TravelersState`.
    Add listeners to detect when the current travel changes and begin loading data accordingly.

--- lib/state/state/ExpensesState.dart
+++ lib/state/state/ExpensesState.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/widgets.dart';
 import 'package:my_travel_app/data/model/travel/shown_travel_basic/shown_travel_basic.dart';
+import 'package:my_travel_app/state/session/shown_travel_session.dart';
 
 import '../../data/repositories/expenses/expense_repository.dart';
 
+/**
+ * これクラス化したほうがいいか？
+ */
 class ExpensesState extends ChangeNotifier {
   final ExpenseRepository _expenseRepository;
+  final ShownTravelSession _travelSession;
 
   ShownTravelBasic? _travel;
 
-  ExpensesState({required ExpenseRepository expensesRepository})
-    : _expenseRepository = expensesRepository {
+  ExpensesState({
+    required ShownTravelSession travelSession,
+    required ExpenseRepository expensesRepository,
+  }) : _travelSession = travelSession,
+       _expenseRepository = expensesRepository {
     print("ExpensesState was created");
+
+    _travelSession.addListener(_onTravelChanged);
+  }
+
+  void _onTravelChanged() async {
+    print("ExpensesState detected travel changed.");
+    if (_travel == _travelSession.currentTravel) return;
+
+    /* ここでロードし始める */
   }
 
   @override
--- lib/state/state/TravlersState.dart
+++ lib/state/state/TravlersState.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:my_travel_app/CommonClass/TravelerBasic.dart';
+import 'package:my_travel_app/data/model/travel/shown_travel_basic/shown_travel_basic.dart';
 import 'package:my_travel_app/data/repositories/participants/travelers_repository.dart';
+import 'package:my_travel_app/state/session/shown_travel_session.dart';
 
 /**
  * 旅行の参加者や役割に紐づく
@@ -10,13 +12,36 @@ import 'package:my_travel_app/data/repositories/participants/travelers_repositor
  * 肥大化してきたら分割する
  */
 class TravelersState extends ChangeNotifier {
+  ShownTravelBasic? _travel;
   Map<String, TravelerBasic>? _participants;
+  String? _generalManager;
+  Map<String, TravelerBasic>? _planners;
 
+  Map<String, TravelerBasic>? get participants => _participants;
+
+  String? get generalManager => _generalManager;
+
+  Map<String, TravelerBasic>? get planners => _planners;
+
+  final ShownTravelSession _travelSession;
   final ParticipantsRepository _participantsRepository;
 
-  TravelersState({required ParticipantsRepository participantsRepository})
-    : _participantsRepository = participantsRepository {
+  TravelersState({
+    required ShownTravelSession travelSession,
+    required ParticipantsRepository participantsRepository,
+  }) : _travelSession = travelSession,
+       _participantsRepository = participantsRepository {
     print("TravelersState was created");
+
+    _travelSession.addListener(_onTravelChanged);
+  }
+
+  void _onTravelChanged() async {
+    print("TravelersState detected travel changed.");
+
+    if (_travel == _travelSession.currentTravel) return;
+
+    /* ここでロードする */
   }
 
   @override
