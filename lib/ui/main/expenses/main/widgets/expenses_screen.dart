03d0e8b2f255736a0c4d5d4450c7146848b2ff9f
GakuAkita <gaku.medaka@gmail.com>
2026-02-28 17:31:19 +0900

    feat: Wrap ExpensesScreen content in a Scaffold
    Updated `ExpensesScreen` to wrap its content in a `Scaffold` widget. This change introduces a placeholder `FloatingActionButton` and ensures the UI structure is consistent with other screens.
    
    Specific changes:
    * Wrapped the existing logic inside `LoadingOverlay` with a `Scaffold`.
    * Added a `FloatingActionButton` that is visible only when the view model is not in a loading state.
    * Adjusted the layout to use the `body` property of the `Scaffold`.

--- lib/ui/main/expenses/main/widgets/expenses_screen.dart
+++ lib/ui/main/expenses/main/widgets/expenses_screen.dart
@@ -26,77 +26,81 @@ class _ExpensesScreenState extends State<ExpensesScreen> {
     final viewModel = context.watch<ExpensesViewModel>();
     return LoadingOverlay(
       isLoading: viewModel.isLoading || !viewModel.travelInitialized,
-      child:
-          viewModel.isLoading
-              ? Center(child: Text("loading..."))
-              : viewModel.currentTravel != null
-              ? RefreshIndicator(
-                onRefresh: () async {
-                  print("^^^^^ExpensesScreen: onRefresh called^^^^^^^");
-                  final result = await viewModel.getAllExpensesWithNotify(
-                    isStateNotify: false,
-                  );
-                  /* 失敗したらSnackBarを出す */
-                },
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          RoundedButton(
-                            title: "割り勘確認",
-                            enabled: viewModel.allExpensesList().isNotEmpty,
-                            onPressed: () {
-                              Navigator.pushNamed(
-                                context,
-                                ExpensesResultScreen.id,
-                              );
-                            },
-                          ),
-                          /* 総監督とAdminだけは見れる */
-                          ...[
-                            SizedBox(width: 10),
+      child: Scaffold(
+        floatingActionButton:
+            viewModel.isLoading ? null : FloatingActionButton(onPressed: () {}),
+        body:
+            viewModel.isLoading
+                ? Center(child: Text("loading..."))
+                : viewModel.currentTravel != null
+                ? RefreshIndicator(
+                  onRefresh: () async {
+                    print("^^^^^ExpensesScreen: onRefresh called^^^^^^^");
+                    final result = await viewModel.getAllExpensesWithNotify(
+                      isStateNotify: false,
+                    );
+                    /* 失敗したらSnackBarを出す */
+                  },
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
                             RoundedButton(
-                              title: "費用概算",
+                              title: "割り勘確認",
+                              enabled: viewModel.allExpensesList().isNotEmpty,
                               onPressed: () {
-                                for (final i
-                                    in viewModel.allGroupMembers.entries) {
-                                  print("${i.value.profile_name}");
-                                }
-                                // Navigator.pushNamed(
-                                //   context,
-                                //   EstimatedExpenseScreen.id,
-                                // );
+                                Navigator.pushNamed(
+                                  context,
+                                  ExpensesResultScreen.id,
+                                );
                               },
                             ),
+                            /* 総監督とAdminだけは見れる */
+                            ...[
+                              SizedBox(width: 10),
+                              RoundedButton(
+                                title: "費用概算",
+                                onPressed: () {
+                                  for (final i
+                                      in viewModel.allGroupMembers.entries) {
+                                    print("${i.value.profile_name}");
+                                  }
+                                  // Navigator.pushNamed(
+                                  //   context,
+                                  //   EstimatedExpenseScreen.id,
+                                  // );
+                                },
+                              ),
+                            ],
                           ],
-                        ],
+                        ),
                       ),
-                    ),
-                    viewModel.allExpensesList().isNotEmpty
-                        ? Expanded(
-                          child: ListView.builder(
-                            itemCount: viewModel.allExpensesList().length,
-                            shrinkWrap: true,
-                            itemBuilder: (context, index) {
-                              return ExpenseTile(
-                                expense: viewModel.allExpensesList()[index],
-                                members:
-                                    viewModel
-                                        .allGroupMembers /* viewModel内でStateを監視して取る */,
-                              );
-                            },
-                          ),
-                        )
-                        : BasicText(text: "費用が何も記録されていません"),
-                  ],
-                ),
-              )
-              : Center(
-                child: BasicText(text: "Settings画面より表示旅行を選択してください。"),
-              ) /* travelが選択されていない */,
+                      viewModel.allExpensesList().isNotEmpty
+                          ? Expanded(
+                            child: ListView.builder(
+                              itemCount: viewModel.allExpensesList().length,
+                              shrinkWrap: true,
+                              itemBuilder: (context, index) {
+                                return ExpenseTile(
+                                  expense: viewModel.allExpensesList()[index],
+                                  members:
+                                      viewModel
+                                          .allGroupMembers /* viewModel内でStateを監視して取る */,
+                                );
+                              },
+                            ),
+                          )
+                          : BasicText(text: "費用が何も記録されていません"),
+                    ],
+                  ),
+                )
+                : Center(
+                  child: BasicText(text: "Settings画面より表示旅行を選択してください。"),
+                ) /* travelが選択されていない */,
+      ),
     );
   }
 }
