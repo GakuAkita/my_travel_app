9b48f4be6a20a56d8d02f3fedb8147b5d570c3a6
GakuAkita <gaku.medaka@gmail.com>
2026-03-20 21:45:16 +0900

    feat: Implement initial layout and basic UI for `GroupCreateScreen`
    This commit adds the core UI components to the `GroupCreateScreen`, including a text field for the group name and a creation button.
    
    Specific changes include:
    *   **GroupCreateScreen**:
        *   Replaced the standard `AppBar` with `TopAppBar`.
        *   Added a `TextEditingController` to manage the group name input and ensured its proper disposal in the `dispose` method.
        *   Structured the body with a `Column` containing a labeled `TextField` for the group name and a `RoundedButton` for group creation.
        *   Added a placeholder comment for displaying the user list.

--- lib/ui/main/settings/group_create/widgets/group_create_screen.dart
+++ lib/ui/main/settings/group_create/widgets/group_create_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_travel_app/components/RoundedButton.dart';
+import 'package:my_travel_app/ui/core/ui/TopAppBar.dart';
 
 class GroupCreateScreen extends StatefulWidget {
   const GroupCreateScreen({super.key});
@@ -8,11 +10,35 @@ class GroupCreateScreen extends StatefulWidget {
 }
 
 class _GroupCreateScreenState extends State<GroupCreateScreen> {
+  final TextEditingController _groupNameController = TextEditingController();
+
+  @override
+  void dispose() {
+    _groupNameController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Create Group')),
-      body: TextField(),
+      appBar: TopAppBar(title: 'Create Group', automaticallyImplyLeading: true),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("グループ名"),
+                TextField(controller: _groupNameController),
+              ],
+            ),
+          ),
+          /* ユーザーを全部表示 */
+          RoundedButton(title: "グループ作成", onPressed: () {}),
+        ],
+      ),
     );
   }
 }
