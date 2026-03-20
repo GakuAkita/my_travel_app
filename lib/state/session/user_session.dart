a6b24aa1395a4cef2d6fd12a7cbb9b53fdbf26f1
GakuAkita <gaku.medaka@gmail.com>
2026-03-20 14:17:17 +0900

    add user session. listen to Realtime Database.

--- lib/state/session/user_session.dart
+++ lib/state/session/user_session.dart
@@ -1,10 +1,23 @@
-import 'package:flutter/foundation.dart';
 
-class UserSession extends ChangeNotifier {
-  @override
-  void dispose() {
-    print("UserSession was disposed");
-    // TODO: implement dispose
-    super.dispose();
-  }
-}
+
+// 一旦いらないか、、
+
+// class UserSession extends ChangeNotifier {
+//   final UserSettingsRepository _userSettingsRepository;
+//
+//   /* 自分のroleやその他ログインで確定するものを保持しておく */
+//   String? _role;
+//
+//   String? get role => _role;
+//
+//   UserSession({required UserSettingsRepository userSettingsRepository})
+//       :_userSettingsRepository=userSettingsRepository
+//
+//
+//   @override
+//   void dispose() {
+//     print("UserSession was disposed");
+//     // TODO: implement dispose
+//     super.dispose();
+//   }
+// }
