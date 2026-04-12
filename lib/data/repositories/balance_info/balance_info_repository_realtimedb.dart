4773f82ee4939f2bff7cf8e3bcf34eefdddce581
GakuAkita <gaku.medaka@gmail.com>
2026-04-12 18:13:01 +0900

    feat: Add `BalanceInfoRepository` and its Realtime Database implementation
    This commit introduces a new repository layer for managing balance information, including an abstract interface and a concrete implementation using Firebase Realtime Database.
    
    Specific changes include:
    *   **BalanceInfoRepository**: Defined an abstract interface with a `getBalanceInfo` method to fetch balance data for a specific group and travel.
    *   **BalanceInfoRepositoryRealtimeDb**:
        *   Implemented the `BalanceInfoRepository` using `FirebaseDatabase`.
        *   Utilized `FirebaseDatabaseService` to fetch balance data from the specific path defined in `FirebaseDatabasePaths`.
        *   Configured JSON serialization/deserialization using the `BalanceInfo` model.

---
+++ lib/data/repositories/balance_info/balance_info_repository_realtimedb.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:my_travel_app/data/firebase_database_paths.dart';
+import 'package:my_travel_app/data/repositories/balance_info/balance_info_repository.dart';
+import 'package:my_travel_app/data/services/firebase_database_service.dart';
+
+import '../../model/balance/balance_info.dart';
+
+class BalanceInfoRepositoryRealtimeDb implements BalanceInfoRepository {
+  final FirebaseDatabase _database;
+
+  BalanceInfoRepositoryRealtimeDb({required FirebaseDatabase database})
+    : _database = database;
+
+  @override
+  Future<Map<String, BalanceInfo>> getBalanceInfo(
+    String groupId,
+    String travelId,
+  ) async {
+    final service = FirebaseDatabaseService(
+      database: _database,
+      path:
+          FirebaseDatabasePaths.group(
+            groupId,
+          ).travels.travel(travelId).expenses.balances,
+      fromJson: BalanceInfo.fromJson,
+      toJson: (e) => e.toJson(),
+    );
+    return await service.getAll();
+  }
+}
